9e16cb97cfff3d7e1e058ca07ece7e464a62c7a1
arpelarpe <rasmus.arpe@gmail.com>
2019-07-16 17:02:20 +0200

    Initial training commit

--- danspeech/audio/parsers.py
+++ danspeech/audio/parsers.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 import torch
 
 import scipy
@@ -8,7 +10,7 @@ windows = {'hamming': scipy.signal.hamming, 'hann': scipy.signal.hann, 'blackman
            'bartlett': scipy.signal.bartlett}
 
 
-class AudioParser(object):
+class AudioParser(ABC):
     """
     Abstract class for AudioParsers
     """
@@ -20,27 +22,34 @@ class AudioParser(object):
 
         # Defaulting if no audio_config
         self.normalize = self.audio_config.get("normalize", True)
-        self.sample_rate = self.audio_config.get("sample_rate", 16000)
+        self.sampling_rate = self.audio_config.get("sample_rate", 16000)
         window = self.audio_config.get("window", "hamming")
         self.window = windows[window]
 
         self.window_stride = self.audio_config.get("window_stride", 0.01)
         self.window_size = self.audio_config.get("window_size", 0.02)
 
+    @abstractmethod
     def parse_audio(self, recording):
-        raise NotImplementedError
+        pass
 
 
 class SpectrogramAudioParser(AudioParser):
 
-    def __init__(self, audio_config=None):
+    def __init__(self, audio_config=None, data_augmenter=None):
         # inits all audio configs
         super(SpectrogramAudioParser, self).__init__(audio_config)
 
-        self.n_fft = int(self.sample_rate * self.window_size)
-        self.hop_length = int(self.sample_rate * self.window_stride)
+        self.n_fft = int(self.sampling_rate * self.window_size)
+        self.hop_length = int(self.sampling_rate * self.window_stride)
+
+        self.data_augmenter = data_augmenter
 
     def parse_audio(self, recording):
+
+        if self.data_augmenter:
+            recording = self.data_augmenter.augment(recording)
+
         # STFT
         D = librosa.stft(recording, n_fft=self.n_fft, hop_length=self.hop_length,
                          win_length=self.n_fft, window=self.window)
---
+++ danspeech/errors/training_errors.py
@@ -0,0 +1,4 @@
+
+
+class ArgumentMissingForOption(Exception):
+    pass
