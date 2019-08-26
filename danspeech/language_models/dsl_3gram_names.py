d8f8e433607ec726373e721b3bca86746e2e2398
arpelarpe <rasmus.arpe@gmail.com>
2019-08-26 19:12:11 +0200

    lm addition

--- danspeech/language_models/dsl_3gram_names.py
+++ danspeech/language_models/dsl_3gram_names.py
@@ -1,11 +1,11 @@
 from danspeech.utils.data_utils import get_model
 
-LANGUAGE_MODEL_ORIGIN = "https://github.com/Rasmusafj/models_development/blob/master/da_lm_3gram_pruned_0_5_30_names.klm"
+LANGUAGE_MODEL_ORIGIN = "https://github.com/Rasmusafj/models_development/raw/master/dsl_names.klm"
 
 
 def DSL3gramWithNames(cache_dir=None):
     return get_model(model_name="dsl_3gram_names.klm",
                      origin=LANGUAGE_MODEL_ORIGIN,
-                     file_hash="33ca3e2a8db3a036af6d7ad85972dbb0",
+                     file_hash="1b47e2db841c6be5c62004ef51a40c68",
                      cache_dir=cache_dir,
                      file_type="language_model")
