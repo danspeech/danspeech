b97906df2804f66582aef6abc644547d3a148574
Rasmus Arpe Fogh Jensen <arpelarpe@Rasmuss-MacBook-Pro-6.local>
2019-08-30 13:40:30 +0200

    Setup changes

--- setup.py
+++ setup.py
@@ -39,19 +39,19 @@ Below is DanSpeech licence
 """
 setuptools.setup(
     name="danspeech",
-    version="0.0.1",
+    version="1.0.0",
     author="Rasmus Arpe Fogh Jensen, Martin Carsten Nielsen",
     author_email="rasmus.arpe@gmail.com, mcnielsen4270@gmail.com,",
     description="Speech recognition for Danish",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rasmusafj/danspeech",
+    url="https://github.com/danspeech/danspeech",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    license_file="LICENCE.txt",
     classifiers=[
         "Programming Language :: Python :: 3",
-        'Development Status :: 3 - Alpha',
-        "License :: OSI Approved :: MIT License",
+        'Development Status :: 3 - Released',
         "Operating System :: OS Independent",
     ],
 )
