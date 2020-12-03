9c05acb6c7ba35abb802589a8cb43d2bd805f01c
Rasmus Arpe Fogh Jensen <rafje@pop-os.localdomain>
2020-12-03 10:31:34 +0100

    IMplemented clean cache method

--- danspeech/__init__.py
+++ danspeech/__init__.py
@@ -1,2 +1,22 @@
+import os
+import warnings
+import shutil
+
 from .Recognizer import Recognizer
 from .DanSpeechRecognizer import DanSpeechRecognizer
+
+
+class NoDefaultCacheDirForDanspeech(Warning):
+    pass
+
+
+def clean_cache():
+    """
+    Method used to clean danspeech cache
+    """
+    cache_dir = os.path.join(os.path.expanduser('~'), '.danspeech')
+    if os.path.isdir(cache_dir):
+        shutil.rmtree(cache_dir)
+    else:
+        warnings.warn("The default cache dir for danspeech (~.danspeech/ did not exist. If you are"
+                      "using custom cache dir, then delete it manually.", NoDefaultCacheDirForDanspeech)
