dc196fb499ef124b020531eb73d802fea6aba9d3
Rasmus Arpe Fogh Jensen <rafje@pop-os.localdomain>
2020-12-04 15:29:07 +0100

    Added helper function for danspeech_training

--- danspeech/pretrained_models/__init__.py
+++ danspeech/pretrained_models/__init__.py
@@ -7,3 +7,24 @@ from .folketinget_model import Folketinget
 from .transfer_learned_model import TransferLearned
 from .librispeech import EnglishLibrispeech
 from .custom_model import CustomModel
+
+
+def get_model_from_string(model_name):
+    if model_name == 'DanSpeechPrimary':
+        return DanSpeechPrimary()
+    elif model_name == 'TestModel':
+        return TestModel()
+    elif model_name == 'Baseline':
+        return Baseline()
+    elif model_name == 'CPUStreamingRNN':
+        return CPUStreamingRNN()
+    elif model_name == 'GPUStreamingRNN':
+        return CPUStreamingRNN()
+    elif model_name == 'Folketinget':
+        return Folketinget()
+    elif model_name == 'TransferLearned':
+        return TransferLearned()
+    elif model_name == 'EnglishLibrispeech':
+        return EnglishLibrispeech()
+    else:
+        return None
